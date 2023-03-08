f76abaad11dca3b0c14e2e9ef2891d5309358478
Lea Sae Cheul <dltpcjf1005@naver.com>
2023-03-08 18:00:04 +0900

    nextapp

---
+++ pages/layout/footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+const Footer=()=>{
+    return(
+        <footer>
+            <hr />
+            <div className="copy">
+                copyright &copy; 2023. qnfzks3. All Rights Reserved.
+            </div>
+        </footer>
+    )
+};
+
+
+export default Footer;
---
+++ pages/layout/header.js
@@ -0,0 +1,17 @@
+
+const Header=()=>{
+    return(
+        <>
+            <h1>JSP 프로젝트 v2</h1>
+            <ul>
+                <li>Home</li>
+                <li>회원가입</li>
+                <li>로그인</li>
+                <li>게시판</li>
+                <li>회원정보</li>
+            </ul>
+        </>
+    )
+}
+
+export default Header;
